62aca39b1f57bf9d9968deb76d7444e35f512c28
BouzieneNacef <bouzaienne1995@gmail.com>
2023-04-07 17:42:56 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+'''firsname = input('write your first name \n')
+lastname = input('write your last name \n')
+print('your name is '+firsname+' ' +lastname)'''
+list = ['a', 15,False,'allez', 22]
+print(list)
+list.append('plus')
+print(list)
+print(list[5])
+print(list[-1])
+print(list[0])
